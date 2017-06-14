8d97b41239f0228f436aff2693a7194b256f3709
Maranatha A. Ilesanmi <maranatha.ilesanmi@andela.com>
2017-06-14 14:49:07 +0100

    [ch #145555901] add JSDoc comments

--- client/components/common/FourOFour.jsx
+++ client/components/common/FourOFour.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+/**
+ * @desc component used to display the 404 page not found.
+ */
 const FourOFour = () =>
   <div>
     <h1>404. Page Not found!</h1>
--- client/components/common/header/MainMenu.jsx
+++ client/components/common/header/MainMenu.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+/**
+ * @desc component used to display the main menu
+ */
 const MainMenu = ({ loggedIn, logout, isAdmin }) =>
   <div>
-      {
-        loggedIn
-          ? <ul className="right hide-on-med-and-down">
-            <li>
-              <Link activeClassName="active" to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <Link activeClassName="active" to="/documents">Documents</Link>
-            </li>
-            {
-              isAdmin === 1
-              ? <li>
-                <Link activeClassName="active" to="/users">Manage Users</Link>
-              </li> : ''
-            }
-            <li>
-              <Link activeClassName="active" to="/logout" onClick={logout}>
-                Logout</Link>
-            </li>
-          </ul>
-        : <ul className="right hide-on-med-and-down">
-            <li>
-              <Link activeClassName="active" to="/">Login</Link>
-            </li>
-            <li>
-              <Link activeClassName="active" to="/signup">Signup</Link>
-            </li>
-          </ul>
-      }
+    {$('.dropdown-button').dropdown()}
+    {
+      loggedIn
+        ? <ul className="right hide-on-med-and-down">
+          <li>
+            <Link activeClassName="active" to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link activeClassName="active" to="/documents">Documents</Link>
+          </li>
+          {
+            isAdmin === 1
+            ? <li>
+              <Link activeClassName="active" to="/users">Manage Users</Link>
+            </li> : ''
+          }
+          <li>
+            <Link activeClassName="active" to="/logout" onClick={logout}>
+              Logout</Link>
+          </li>
+        </ul>
+      : <ul className="right hide-on-med-and-down">
+          <li>
+            <Link activeClassName="active" to="/">Login</Link>
+          </li>
+          <li>
+            <Link activeClassName="active" to="/signup">Signup</Link>
+          </li>
+        </ul>
+    }
 
     {
       loggedIn
@@ -65,6 +69,9 @@ const MainMenu = ({ loggedIn, logout, isAdmin }) =>
   </div>
 ;
 
+/**
+ * @desc Set the PropTypes
+ */
 MainMenu.propTypes = {
   logout: React.PropTypes.func,
   isAdmin: React.PropTypes.number,
